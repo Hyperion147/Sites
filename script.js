31c75cbd1dd4300c2a4f3f48ed1eb45b8899e3d7
hyperion9913 <supernoob7903@gmail.com>
2024-08-21 21:55:42 +0530

    time

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const clock = document.querySelector('#clock')
 setInterval(function(){
     let date = new Date();
     clock.innerHTML = date.toLocaleTimeString();
-    console.log(date);
     
 }, 1000);
 
