1d4954821a19e8c2b42a5b6ce8b86486462e8452
hyperion9913 <supernoob7903@gmail.com>
2024-08-21 19:41:57 +0530

    hello

--- 1.js
+++
@@ -1,8 +0,0 @@
-let a = 5;
-let b = 10;
-
-function addValues() {
-    console.log(`The sum is = ${a + b}`);
-}
-
-addValues()
--- app.js
+++
@@ -1,5 +0,0 @@
-const path = require('path')
-
-console.log(path.sep);
-const dir = path.resolve(__dirname)
-console.log(dir);
