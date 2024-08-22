87a20444533b6d2b4cdc2544d1a22041477c5143
hyperion9913 <supernoob7903@gmail.com>
2024-08-22 19:01:13 +0530

    incorrect guessGame

--- script.js
+++ script.js
@@ -1,8 +1,71 @@
-const clock = document.querySelector('#clock')
+const ranNum = parseInt(Math.random() * 100 + 1);
 
-setInterval(function(){
-    let date = new Date();
-    clock.innerHTML = date.toLocaleTimeString();
-    
-}, 1000);
+const guesses = document.querySelector('#guess');
+const submit = document.querySelector('#subt');
+const guessField = document.querySelector('#guessField');
+const guessNo = document.querySelector('.guesses');
+const remResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
 
+const p = document.createElement('p');
+
+let prevGuess = [];
+let numGuess = 1;
+
+let playGame = true;
+
+if (playGame) {
+    submit.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const guess = parseInt(guessField.value);
+        validateGuess(guess)
+    })
+}
+
+
+function validateGuess(guess) {
+    if (guess < 1 || guess > 100 || isNaN(guess)) {
+        alert("Please Enter a number between 1 and 100")
+    }
+    else {
+        if (guesses === 11) {
+            displayGuess(guess)
+            displayMessage(`Game Over`)
+            endGame()
+        }
+        else {
+            displayGuess()
+            checkGuess()
+        }
+    }
+}
+
+function checkGuess(guess){
+    if(guess === ranNum){
+        displayMessage(`Good`);
+        endGame();
+    }
+    else if(guess > ranNum){
+        displayMessage("LOWWW")
+    }
+    else if(guess < ranNum){
+        displayMessage("HIGHHH")
+    }
+}
+function displayGuess(guess){
+    guessField.value = ''
+    guesses.innerHTML += `${guess}`
+    numGuess++;
+    remResult.innerHTML = `${11 - guess}`
+}
+function displayMessage(message){
+    lowOrHi.innerHTML = `<h3>${message}</h3>`
+}
+function endGame(){
+
+}
+function newGame(){
+
+}
