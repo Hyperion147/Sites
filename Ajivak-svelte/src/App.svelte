dc6499a364f7648f924e997cafc43790f2409a99
hyperion9913 <supernoob7903@gmail.com>
2025-03-13 15:04:00 +0530

    clean

--- Ajivak-svelte/src/App.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import Header from './components/Header.svelte';
-    import MainContent from './components/MainContent.svelte';
-    import Footer from './components/Footer.svelte';
-</script>
-
-<Header />
-<MainContent />
-<Footer />
